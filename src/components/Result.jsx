7fdb80b6c3049a5079928df49d3b7f90bfdd0dfa
fatima <fatima.achims@gmail.com>
2024-11-14 21:02:34 +0100

    handle the error

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,10 +4,10 @@ const Result = ({ results }) => {
   return (
     <div>
        {results.map((result) => (
-        <div key={result.id} style={{ marginBottom: "20px" }}>
+        <div key={result.id} style={{ marginBottom: "10px" }}>
           <h2>{result.original_title || result.name}</h2>
-          <p>{result.overview}</p>
-          <p>Release Date: {result.release_date || result.first_air_date}</p>
+          {/* <p>{result.overview}</p>
+          <p>Release Date: {result.release_date || result.first_air_date}</p> */}
         </div>
       ))}
     </div>
