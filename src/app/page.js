b46e7ecbcd1b251e8d093757d7b45dae1ec09479
fatima <fatima.achims@gmail.com>
2024-11-14 21:32:24 +0100

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import Result from "@/components/Result";
 
 const API_KEY = process.env.API_KEY;
+
 const Home = async ({ searchParams }) => {
   const genre = searchParams.genre || "fetchTrending";
+
+  // Add a loading state with a timeout
+  let loaderVisible = true;
+  
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
@@ -14,6 +20,11 @@ const Home = async ({ searchParams }) => {
   }
   const results = data.results;
   console.log(results)
+
+  // Hide the loader after a small delay
+  setTimeout(() => {
+    loaderVisible = false; // Hide loader after timeout
+  }, 2000); // Set the timeout to hide the loader after 500ms (adjust as needed)
 
   return( 
   <div>
